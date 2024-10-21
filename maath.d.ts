16e45d3a04023f280af7b7bf61010e5e4d772fb2
ValentinaPaladines <val.paladinezunal@gmail.com>
2024-10-21 08:13:38 -0500

    maathFuntionsUpdated

---
+++ maath.d.ts
@@ -0,0 +1,6 @@
+declare module 'maath/random/dist/maath-random.esm' {
+  export function someFunction(...args: any[]): any;
+  export function inSphere(array: Float32Array, options?: { radius?: number }): Float32Array;
+   // Ajusta según las funciones que necesites
+  // Añade más funciones según lo que uses del módulo
+}
